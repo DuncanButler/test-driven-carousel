50123f9663a8a01b4ffade0dc0dc37c0fda83ab9
Duncan Butler <duncan.butler@outlook.com>
2021-02-23 18:11:50 +0000

    :sparkles: Add prop for extending the <img> component

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types'
 import CarouselButton from './CarouselButton'
 import CarouselSlide from './CarouselSlide'
 
-class Carousel extends React.PureComponent {
+export default class Carousel extends React.PureComponent {
   static propTypes = {
+    defaultImg: CarouselSlide.propTypes.Img,
     defaultImgHeight: CarouselSlide.propTypes.imgHeight,
     slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes)).isRequired,
   }
 
   static defaultProps = {
+    defaultImg: CarouselSlide.defaultProps.Img,
     defaultImgHeight: CarouselSlide.defaultProps.imgHeight,
   }
 
@@ -32,18 +34,22 @@ class Carousel extends React.PureComponent {
   }
 
   render() {
-    const { defaultImgHeight, slides, ...rest } = this.props
+    const { defaultImg, defaultImgHeight, slides, ...rest } = this.props
 
     return (
       < div {...rest}>
         <CarouselSlide
+          Img={defaultImg}
           imgHeight={defaultImgHeight}
-          {...slides[this.state.slideIndex]} />
-        <CarouselButton data-action="prev" onClick={this.handlePrevClick}>Prev</CarouselButton>
-        <CarouselButton data-action="next" onClick={this.handleNextClick}>Next</CarouselButton>
+          {...slides[this.state.slideIndex]}
+        />
+        <CarouselButton data-action="prev" onClick={this.handlePrevClick}>
+          Prev
+        </CarouselButton>
+        <CarouselButton data-action="next" onClick={this.handleNextClick}>
+          Next
+        </CarouselButton>
       </div >
     )
   }
 }
-
-export default Carousel
